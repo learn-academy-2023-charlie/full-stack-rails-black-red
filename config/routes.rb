44aa7a5d3b8a79a7f234d65e0f2ff918bbbced0d
m3vance <m3vance@gmail.com>
2023-05-24 16:52:46 -0700

    completed story 1

--- config/routes.rb
+++ config/routes.rb
@@ -1,4 +1,6 @@
 Rails.application.routes.draw do
+  get 'blogs' => 'blog#index'
+  root "blog#index"
   # Define your application routes per the DSL in https://guides.rubyonrails.org/routing.html
 
   # Defines the root path route ("/")
